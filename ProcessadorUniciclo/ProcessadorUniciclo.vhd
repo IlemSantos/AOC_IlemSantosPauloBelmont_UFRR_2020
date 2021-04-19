0d094c913beabc44ebe3fc4d0fe52b6713d4aba6
Ilem Santos <ilemlima@gmail.com>
2021-04-19 13:13:32 -0400

    Criação do Projeto no quartus

---
+++ ProcessadorUniciclo/ProcessadorUniciclo.vhd
@@ -0,0 +1,14 @@
+LIBRARY ieee ;
+USE ieee.std_logic_1164.all ;
+
+ENTITY ProcessadorUniciclo IS
+	PORT (
+	
+	);
+END ProcessadorUniciclo ;
+
+ARCHITECTURE Behavior OF ProcessadorUniciclo IS
+
+BEGIN
+
+END Behavior ; 
